76960e66c4952936a5b724b2e87b74eab2f2ae7e
loup <loup.quehec@gmail.com>
2025-02-09 21:25:34 +0100

    init

--- app/(app)/auth/login/index.tsx
+++ app/(app)/auth/login/index.tsx
@@ -1,6 +1,7 @@
 import { ImageBackground, StyleSheet } from 'react-native';
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from '@/components/ThemedText';
+import { Icons } from '@/constants/Icons';
 
 const backgroundImages = [
 	require('@/assets/images/auth/login/background/1.gif'),
@@ -11,6 +12,7 @@ const Login = () => {
 		<ThemedView style={styles.container}>
 			<ImageBackground source={backgroundImages[0]} style={StyleSheet.absoluteFill} />
 			<ThemedView style={styles.card}>
+				<Icons.site.logo width={128} height={128} color='#fff' style={{ color: '#fff' }} className='text-red-500' />
 				<ThemedText type="title">Welcome back</ThemedText>
 			</ThemedView>
 		</ThemedView>
