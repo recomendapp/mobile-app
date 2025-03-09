cc81f3f33b67b315e0c9f056f0b0e0d67ffada35
loup <loup.quehec@gmail.com>
2025-03-09 23:39:40 +0100

    starting film page with parallax effect

---
+++ components/ui/AnimatedImageWithFallback.tsx
@@ -0,0 +1,4 @@
+import Animated from 'react-native-reanimated';
+import { ImageWithFallback } from '../utils/ImageWithFallback';
+
+export const AnimatedImageWithFallback = Animated.createAnimatedComponent(ImageWithFallback);
--- components/utils/ImageWithFallback.tsx
+++ components/utils/ImageWithFallback.tsx
@@ -1,47 +1,75 @@
-'use client';
+import * as React from 'react';
 import { ComponentProps, useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
 import { MediaType } from '@/types/type.db';
 import { DimensionValue, Text, View } from 'react-native';
 import { ImageIcon, ListVideoIcon } from 'lucide-react-native';
 import { Icons } from '@/constants/Icons';
-import { Image, ImageSource } from 'expo-image';
+import { Image, ImageProps, ImageSource } from 'expo-image';
 import deepmerge from 'deepmerge';
+import tailwind from 'twrnc';
 
-interface ImageWithFallbackProps extends ComponentProps<typeof Image> {
+interface ImageWithFallbackProps extends ComponentProps<typeof View> {
+  source: ImageSource;
+  contentFit?: ImageProps['contentFit'];
+  transition?: ImageProps['transition'];
+  cachePolicy?: ImageProps['cachePolicy'];
   alt: string;
   type?: 'default' | 'playlist' | 'service' | 'watch-provider' | MediaType | null;
-  blurDataURL?: string;
 }
 
 // const blurhash =
 //   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 
-export const ImageWithFallback = ({
+export const ImageWithFallback = React.forwardRef<
+  View,
+  ImageWithFallbackProps
+>(({
   source,
   alt,
   type = 'default',
-  blurDataURL,
   className,
   contentFit = 'cover',
   transition = 250,
   cachePolicy = 'disk',
   style = {},
   ...rest
-}: ImageWithFallbackProps) => {
+}, ref) => {
   const [imgSrc, setImgSrc] = useState<ImageSource | undefined | null>(source);
-
-  const defaultStyle = { width: '100%' as DimensionValue, height: '100%' as DimensionValue };
 
   useEffect(() => {
     setImgSrc(source);
   }, [source]);
 
   return (
-    <>
+    <View
+    ref={ref}
+    className={cn('overflow-hidden items-center justify-center w-full h-full bg-muted rounded-md', className)}
+    style={[
+      style,
+    ]}
+    {...rest}
+    >
       {imgSrc ? (
         <Image
+        source={imgSrc}
+        onError={() => {
+          setImgSrc(null);
+        }}
+        style={
+          tailwind.style('w-full h-full')
+        }
+        transition={transition}
+        contentFit={contentFit}
+        cachePolicy={cachePolicy}
+        />
+      ) : (
+        <Fallback type={type} alt={alt} />
+      )}
+      {/* {imgSrc ? (
+        <Image
+          ref={ref}
           source={imgSrc}
           onError={() => {
             setImgSrc(null);
@@ -55,50 +83,30 @@ export const ImageWithFallback = ({
         />
       ) : (
         <Fallback className={cn('', className)} type={type} from="#363636" to="#363636" alt={alt} />
-      )}
-    </>
-  );
-};
-
-export function Fallback({
-  className,
-  type,
-  from,
-  to,
-  alt,
-}: {
-  className?: string;
-  type?: string | null;
-  from: string;
-  to: string;
-  alt?: string;
-}) {
-  return (
-    <View
-      style={{
-        backgroundImage: `linear-gradient(to top right, ${from}, ${to})`,
-      }}
-      className={cn(`w-full h-full flex items-center justify-center bg-muted`, className)}
-    >
-      {type == 'playlist' ? (
-        <ListVideoIcon color="#fff" className="w-2/5 h-2/5" />
-      ) : type == 'person' ? (
-        <Icons.user color="#fff" className="w-2/5 h-2/5" />
-      ) : type == 'movie' ? (
-        <Text className="text-muted-foreground text-clamp-2 text-center">
-          {alt}
-        </Text>
-      ) : type == 'tv_series' ? (
-        <Text className="text-muted-foreground text-clamp-2 text-center">
-          {alt}
-        </Text>
-      ) : type == 'service' ? (
-        <Text className="text-muted-foreground text-clamp-2 text-center">
-          {alt}
-        </Text>
-      ) : (
-        <ImageIcon color="#fff" className="w-2/5 h-2/5" />
-      )}
+      )} */}
     </View>
   );
-}
+});
+
+const Fallback = ({
+  type,
+  alt,
+}: {
+  type?: string | null;
+  alt?: string;
+}) => {
+  switch (type) {
+    case 'playlist':
+      return <ListVideoIcon color="#fff" className="w-2/5 h-2/5" />;
+    case 'person':
+      return <Icons.user color="#fff" className="w-2/5 h-2/5" />;
+    case 'movie':
+      return <Text className="text-muted-foreground text-clamp-2 text-center">{alt}</Text>;
+    case 'tv_series':
+      return <Text className="text-muted-foreground text-clamp-2 text-center">{alt}</Text>;
+    case 'service':
+      return <Text className="text-muted-foreground text-clamp-2 text-center">{alt}</Text>;
+    default:
+      return <ImageIcon color="#fff" className="w-2/5 h-2/5" />;
+  }
+};
