57d9d6939d97b3384e9f3d51ef26b5af8df49632
loup <loup.quehec@gmail.com>
2025-03-13 19:02:00 +0100

    no idea what im doing

---
+++ app/(tabs)/(home,search,collection)/settings/security.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from "@/components/ui/ThemedText";
+import Animated from "react-native-reanimated";
+
+const SecuritySettings = () => {
+	return (
+		<Animated.ScrollView>
+			<ThemedText>Security settings</ThemedText>
+		</Animated.ScrollView>
+	)
+};
+
+export default SecuritySettings;
--- components/nav/NavSettings.tsx
+++ components/nav/NavSettings.tsx
@@ -1,8 +1,9 @@
 import { Link, LinkProps, usePathname } from "expo-router";
 import { useTranslation } from "react-i18next";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { FlatList, View } from "react-native";
 import tw from "@/lib/tw";
 import { useTheme } from "@/context/ThemeProvider";
+import { Button, ButtonText } from "../ui/Button";
 
 const NavSettings = () => {
 	const { t } = useTranslation();
@@ -10,25 +11,25 @@ const NavSettings = () => {
 
 	const NavSettingsItems: { title: string; href: LinkProps['href'] }[] = [
 		{
-		title: t('profile.label'),
+		title: t('pages.settings.profile.label'),
 		href: '/settings/profile',
 		},
 		{
-		title: t('account.label'),
+		title: t('pages.settings.account.label'),
 		href: '/settings/account',
 		},
 		{
-		title: t('appearance.label'),
+		title: t('pages.settings.appearance.label'),
 		href: '/settings/appearance',
 		},
 		{
-		title: t('subscription.label'),
+		title: t('pages.settings.subscription.label'),
 		href: '/settings/subscription',
 		},
-		// {
-		// title: t('security.label'),
-		// href: '/settings/security',
-		// },
+		{
+		title: t('pages.settings.security.label'),
+		href: '/settings/security',
+		},
 		// {
 		// title: t('notifications.label'),
 		// href: '/settings/notifications',
@@ -42,7 +43,7 @@ const NavSettings = () => {
 		<View>
 			<FlatList
 			data={NavSettingsItems}
-			contentContainerStyle={tw.style('gap-2')}
+			contentContainerStyle={tw.style('gap-2 px-2')}
 			renderItem={({ item }) => (
 				<NavSettingsItem title={item.title} href={item.href} active={pathname === item.href} />
 			)}
@@ -68,9 +69,9 @@ const NavSettingsItem = ({
 		href={href}
 		asChild
 		>
-			<Pressable style={[tw.style('rounded-full'), active && { backgroundColor: colors.muted }]}>
-				<Text>{title}</Text>
-			</Pressable>
+			<Button variant={active ? 'default' : 'outline'} style={{ borderRadius: 9999}}>
+				<ButtonText variant={active ? 'default' : 'outline'}>{title}</ButtonText>
+			</Button>
 		</Link>
 	)
 };
