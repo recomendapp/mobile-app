a777c1e64a8f15c34e17853c8ddb581226e98d95
loup <loup.quehec@gmail.com>
2025-11-07 19:53:33 +0100

    adding onboarding video, remove lottie

--- src/screens/onboarding/data.ts
+++ src/screens/onboarding/data.ts
@@ -1,9 +1,10 @@
+import { Assets } from '@/constants/Assets';
 import { upperFirst } from 'lodash';
 import { useTranslations } from 'use-intl';
 
 export interface OnboardingData {
   id: number;
-  animation: string;
+  video: string;
   text: string;
   textColor: string;
   backgroundColor: string;
@@ -14,28 +15,28 @@ const useData = ((): OnboardingData[] => {
   return [
     {
       id: 1,
-      animation: require('@/assets/animations/onboarding/Lottie1.json'),
+      video: Assets.onboarding.tracking,
       text: upperFirst(t('pages.onboarding.tracking.description')),
       textColor: '#005b4f',
       backgroundColor: '#ffa3ce',
     },
     {
       id: 2,
-      animation: require('@/assets/animations/onboarding/Lottie2.json'),
+      video: Assets.onboarding.recos,
       text: upperFirst(t('pages.onboarding.recos.description')),
       textColor: '#1e2169',
       backgroundColor: '#bae4fd',
     },
     {
       id: 3,
-      animation: require('@/assets/animations/onboarding/Lottie3.json'),
+      video: Assets.onboarding.playlists,
       text: upperFirst(t('pages.onboarding.playlists.description')),
       textColor: '#F15937',
       backgroundColor: '#faeb8a',
     },
     {
       id: 4,
-      animation: require('@/assets/animations/onboarding/Lottie3.json'),
+      video: Assets.onboarding.tracking,
       text: upperFirst(t('pages.onboarding.social.description')),
       textColor: '#004f91',
       backgroundColor: '#d5afff',
